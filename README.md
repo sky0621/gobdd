1d223c91bd8679508c0bf04d0488bb3576493f88
sky0621 <sky14work@gmail.com>
2019-04-30 01:51:29 +0900

    continue

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 https://onsi.github.io/ginkgo/
 
+$ ginkgo #or go test
+
+## generate test file
+
+$ ginkgo generate notice
---
+++ notice_test.go
@@ -0,0 +1,16 @@
+package gobdd_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Notice", func() {
+	Describe("『お知らせ』の登録", func() {
+		Context("主体が「システム管理者」である場合", func() {
+			It("表示期間を指定して『お知らせ』を登録できる。", func() {
+				Expect().To(Equal())
+			})
+		})
+	})
+})
